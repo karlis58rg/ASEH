987d89791b2d50be3416509bc8266c7346dc2b6b
karlis58rg@gmail.com <karlis58rg@gmail.com>
2020-05-08 17:23:17 -0500

    CarruselExcepción

--- app/src/main/java/mx/hgo/aseh/ui/tools/ToolsFragment.java
+++ app/src/main/java/mx/hgo/aseh/ui/tools/ToolsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -12,24 +13,45 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
+
 import mx.hgo.aseh.R;
+import mx.hgo.aseh.ui.gallery.GalleryViewModel;
 
 public class ToolsFragment extends Fragment {
 
     private ToolsViewModel toolsViewModel;
+    private GalleryViewModel galleryViewModel;
+
+    CarouselView baner;
+    int[] carrusel = {R.drawable.caso_uno,R.drawable.caso_dos};
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         toolsViewModel =
                 ViewModelProviders.of(this).get(ToolsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_tools, container, false);
-        final TextView textView = root.findViewById(R.id.lblCasosExcepcion);
+        //*************************** BANER **********************************************/
+        baner = (CarouselView) root.findViewById(R.id.banerLegal);
+        baner.setPageCount(carrusel.length);
+        baner.setImageListener(imageListener);
+
+
+        /*********************************************************************************/
         toolsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
             }
         });
         return root;
     }
+
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(carrusel[position]);
+
+        }
+    };
 }
--- app/src/main/java/mx/hgo/aseh/ui/tools/ToolsViewModel.java
+++ app/src/main/java/mx/hgo/aseh/ui/tools/ToolsViewModel.java
@@ -10,33 +10,6 @@ public class ToolsViewModel extends ViewModel {
 
     public ToolsViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("VII. Vehículos particulares en los que se trasladen personas que realicen actividades esenciales, así determinadas por las autoridades sanitarias competentes . . . \n" +
-                "Financieros (eje. Bancos). \n" +
-                "Recaudación tributaria (SAT, SFPH).\n" +
-                "Distribución y venta de energéticos (Eje. Oxifuel).\n" +
-                "Gasolineras y gas.\n" +
-                "Generación y distribución de agua potable (eje. pipas).\n" +
-                "Industria de alimentos y bebidas no alcohólicas. \n" +
-                "Mercados de alimentos.\n" +
-                "Supermercados.\n" +
-                "Tiendas de autoservicio. \n" +
-                "Abarrotes \n" +
-                "Venta de alimentos preparados (eje. Pastes).\n" +
-                "Producción agrícola (eje, tractores y maquinaria de siembra).\n" +
-                "Pesquera y pecuaria (traslado de animales aptos para consumo).\n" +
-                "Industria química (eje. \n" +
-                "Productos de limpieza.\n" +
-                "Ferreterías.\n" +
-                "Servicios de mensajería.\n" +
-                "Guardias en labores de seguridad privada. \n" +
-                "Guarderías y estancias infantiles.\n" +
-                "Asilos y estancias para personas adultas mayores.\n" +
-                "Refugios y centros de atención a mujeres víctimas de violencia, sus hijas e hijos.\n" +
-                "Telecomunicaciones y medios de información (Telmex)\n" +
-                "Soporte de Herramientas de Seguridad Pública (eje. Seguritech) \n" +
-                "Servicios privados de emergencia (ambulancias privadas). \n" +
-                "Servicios funerarios y de inhumación. \n" +
-                "Servicios de almacenamiento de insumos esenciales.");
     }
 
     public LiveData<String> getText() {
